aebbcb13c3edb01b88e9fc7854967a6ab5ca41c2
tenma043 <tenten25420@gmail.com>
2019-11-22 17:55:12 +0700

    test63

---
+++ functions/public/home.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data (){
+        return {
+            login:false,
+            register:false,
+            e1:0,
+        }
+    },
+})
