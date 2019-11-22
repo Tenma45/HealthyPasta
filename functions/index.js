89f43728ad3100c046b08816bbe37575aaabf2f5
tenma043 <tenten25420@gmail.com>
2019-11-22 22:49:14 +0700

    Signed-off-by: tenma043 <tenten25420@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,30 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const path = require('path')
+const bodyParser = require('body-parser')
 const app = express();
-var site_root = path.resolve(__dirname+'/..');
+var firebase = require('firebase-admin');
+var serviceAccount = require('./serviceAccountKey.json')
+
+app.use(bodyParser());
+
+firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: 'https://realtimeapp-b69e4.firebaseio.com'
+  });
+  
+const database = firebase.database();
+const memberref = database.ref('member')
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/public/home.html'));
+})
+
+app.post('/register',(req,res)=>{
+    console.log(req.body.name)
+    memberref.push(req.body)
+    res.send("Success register");
+
 })
 
 app.get('/time',(req,res)=>{
