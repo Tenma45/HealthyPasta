5bfa8542a76a831486a3926f9270b66228db94bd
tenma043 <tenten25420@gmail.com>
2019-11-23 14:18:46 +0700

    Register Check Beta

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,13 @@ const functions = require('firebase-functions');
 const express = require('express');
 const path = require('path')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const app = express();
 var firebase = require('firebase-admin');
 var serviceAccount = require('./serviceAccountKey.json')
 
+app.use(cookieParser())
 app.use(bodyParser());
 
 firebase.initializeApp({
@@ -22,18 +25,18 @@ app.get('/',(req,res)=>{
 
 app.post('/register',(req,res)=>{
     req.body.role="client"
-    memberref.push(req.body)
+    memberref.child("/"+req.body.username).set(req.body)
     res.sendFile(path.join(__dirname,'/public/UserIndex.html'));
 
 })
 
 app.get('/error',(req,res)=>{
+    
     res.sendFile(path.join(__dirname,'/public/404.html'));
 })
 
 app.listen(process.env.port,()=>{
-    console.log("Server on "+process.env.port);
-}
-)
+    console.log("Server is on");
+})
 
 exports.app = functions.https.onRequest(app);
--- functions/public/home.js
+++ functions/public/home.js
@@ -1,39 +1,76 @@
 new Vue({
     el: '#app',
     vuetify: new Vuetify(),
-    data (){
-        return {
-            login:false,
-            register:false,
-            e1:0,
-        }
-    },
-})
+    data :{
+        username: null,
+        password: null,
+        firstname: null,
+        lastname: null,
+        bod: null,
+        email: null,
+        address: null,
+        tel: null,
+        blood: null,
+        weight: null,
+        height: null,
+        login:false,
+        register:false,
+        e1:0,
+        errors: []       
+        },
+    methods: {
+        /*registerOn() {
+            alert("TEST")
+            axios.post('/register2', {
+                email: this.email,
+                username: this.username
+            })
+        },*/
+        checkForm1: function (e) {
+            
+            this.errors = [];
+      
+            if (!this.username) {
+              this.errors.push('Username required.');
+            }
+            if (!this.password) {
+              this.errors.push('Password required.');
+            }
+            if(this.errors.length===0){
+                this.e1=2;
+            }
+          },
+        checkForm2: function (e) {
+            
+            this.errors = [];
+            
+            if (!this.firstname) {
+                this.errors.push('Name required.');
+            }
 
-new Vue({
-    el: '#form1',
-    data: {
-        errors: [],
-        name: null,
-        age: null,
-        movie: null
-      },
-      methods:{
-        checkForm: function (e) {
-          if (this.name && this.age) {
+            if(this.errors.length===0){
+                return true;
+            } 
+
+            e.preventDefault();
+        },
+        resetForm: function (e) {
+            this.username= null
+            this.password= null
+            this.firstname= null
+            this.lastname= null
+            this.bod= null
+            this.email= null
+            this.address= null
+            this.tel= null
+            this.blood= null
+            this.weight= null
+            this.height= null
+            this.login=false
+            this.register=false
+            this.e1=0
+            this.errors= []     
             return true;
-          }
-    
-          this.errors = [];
-    
-          if (!this.name) {
-            this.errors.push('Name required.');
-          }
-          if (!this.age) {
-            this.errors.push('Age required.');
-          }
-    
-          e.preventDefault();
-        }
-      }
-    })
+        }    
+    }
+})
