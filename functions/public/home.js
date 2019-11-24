f6d0d45c3a33560e68fc1741f119271d989a0729
Gruvia <Audtachai_love@hotmail.com>
2019-11-24 13:40:40 +0700

    add Course in home round 2 and add no-image in folder

--- functions/public/home.js
+++ functions/public/home.js
@@ -1,5 +1,63 @@
+const Home={template:`<v-container> 
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+    ></v-carousel-item>
+  </v-carousel>
+</template>
+</v-container>`}
+
+const Course={template:`<v-container>
+<div align="center"><img src="./pic/course-1.png" height="300"></div>
+<div class="white--text" style="text-decoration: underline; font-size: 24px;" > เลือก Course ที่ใช่ด้วยใจที่ชอบ</div>
+<v-card color="black" outlined  max-width="1200">
+  <v-card-text class="white--text subtitle-1">
+        <v-row>
+            <v-col col="12" sm="3">
+                <div class="text-center"><img src="./pic/no-image-available-icon-6.jpg" height="150"></div>
+            </v-col>
+
+            <v-col col="12" sm="7">
+            รายละเอียด : คอร์สนี้เป็นบลาๆสำหรับที่จะทำนู่นนี่นั่นเพื่อที่จะทำให้ร่างกายแข็งแรงและมีสุขภาพที่ดีเหมือนคนอื่นๆทั่วไป
+            </v-col>
+
+            <v-col col="12" sm="2">
+            <v-card-actions>
+                <v-dialog v-model="choose2" width="500" >
+                    <template v-slot:activator="{ on }">
+                        <v-btn color="#FDA50B"  x-large class="mx-auto" outlined depressed v-on="on" >
+                            <span class="title font-weight-bold" >เลือก</span>
+                        </v-btn>
+                    </template>
+                    <v-card >
+                        <v-card-text class="black--text pt-5 text-center">
+                            <span style="font-size: 24px;">กรุณาเข้าสู่ระบบก่อนเลือก Course</span>
+                            <div><v-icon size=120 color="#FDA50B">warning</v-icon></div>
+                        </v-card-text>
+                    </v-card>
+                </v-dialog>
+            </v-card-actions>
+            </v-col>
+        </v-row>
+    </v-card-text>
+  </v-card></v-container>`}
+
+const router=new VueRouter ({
+    routes : [
+        {path:'', component:Home},
+        {path:'/home', component:Home},
+        {path:'/course', component:Course},
+            
+        
+    ]
+})
+
 new Vue({
     el: '#app',
+    router,
     vuetify: new Vuetify(),
     data : function () {
         return {
@@ -33,6 +91,7 @@ items: [
             login:false,
             register:false,
             e1:0,
+            choose2:false,
             required: function(v) { return v.length >0 || "This field is required"}, 
             minimum: v => v.length >=5 || "At least 5 characters",
             rules: {
